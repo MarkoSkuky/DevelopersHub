21a4374ff447efaa00d6788f68badcf703df7239
Marko Skukálek <markoskukalek1@gmail.com>
2026-05-17 22:16:36 +0200

    Add developer controller

---
+++ backend/src/main/java/com/marko/backend/controller/DeveloperController.java
@@ -0,0 +1,38 @@
+package com.marko.backend.controller;
+
+import com.marko.backend.service.DeveloperService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/developers")
+@RequiredArgsConstructor
+public class DeveloperController {
+    private final DeveloperService developerService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DeveloperResponse> getDeveloperById(@PathVariable Long id) {
+        return ResponseEntity.ok(developerService.getDeveloperById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DeveloperResponse>> getAllDevelopers() {
+        return ResponseEntity.ok(developerService.getAllDevelopers());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removeDeveloperById(@PathVariable Long id) {
+        developerService.removeDeveloperById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<DeveloperResponse> createNewDeveloper(@Valid @RequestBody CreateDeveloperRequest request) {
+        DeveloperResponse response = developerService.createDeveloper(request);
+        return ResponseEntity.status(201).body(response);
+    }
+}
---
+++ backend/src/main/java/com/marko/backend/service/DeveloperService.java
@@ -0,0 +1,8 @@
+package com.marko.backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeveloperService {
+
+}
